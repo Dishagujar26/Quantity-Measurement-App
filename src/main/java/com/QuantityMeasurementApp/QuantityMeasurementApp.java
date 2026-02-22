207b24de342ffcaabb9ce7e7b6abbdd8f4e715c8
Dishagujar26 <dishagujar1@gmail.com>
2026-02-22 22:58:55 +0530

    [Disha] : Modified QuantityMeasurementApp file

--- src/main/java/com/QuantityMeasurementApp/QuantityMeasurementApp.java
+++ src/main/java/com/QuantityMeasurementApp/QuantityMeasurementApp.java
@@ -4,9 +4,9 @@ public class QuantityMeasurementApp {
 
 	public static void main(String[] args) {
 
-		Feet f1 = new Feet(1.0);
-		Feet f2 = new Feet(1.0);
+		Length length1 = new Length(1.0, LengthUnit.FEET);
+		Length length2 = new Length(12.0, LengthUnit.INCHES);
 
-		System.out.println("Equal: " + f1.equals(f2));
+		System.out.println("Are lengths equal? " + length1.equals(length2));
 	}
 }
