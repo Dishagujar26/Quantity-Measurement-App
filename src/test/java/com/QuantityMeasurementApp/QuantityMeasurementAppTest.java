21bab4a67ba1b18f813c3fad7a3ea7d55a0e8c7f
Dishagujar26 <dishagujar1@gmail.com>
2026-02-22 22:26:26 +0530

    [Disha Gujar] : Added QuantityMeasurementAppTest file

---
+++ src/test/java/com/QuantityMeasurementApp/QuantityMeasurementAppTest.java
@@ -0,0 +1,85 @@
+package com.QuantityMeasurementApp;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class QuantityMeasurementAppTest {
+
+    @Test
+    public void testFeetEquality_SameValue() {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+
+        assertTrue(f1.equals(f2), "1.0 ft should be equal to 1.0 ft");
+    }
+
+    @Test
+    public void testFeetEquality_DifferentValue() {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(2.0);
+
+        assertFalse(f1.equals(f2), "1.0 ft should not be equal to 2.0 ft");
+    }
+
+    @Test
+    public void testFeetEquality_NullComparison() {
+        Feet f1 = new Feet(1.0);
+
+        assertFalse(f1.equals(null), "Feet object should not be equal to null");
+    }
+
+    @Test
+    public void testFeetEquality_DifferentClass() {
+        Feet f1 = new Feet(1.0);
+        String other = "1.0";
+
+        assertFalse(f1.equals(other), "Feet object should not be equal to different type");
+    }
+
+    @Test
+    public void testFeetEquality_SameReference() {
+        Feet f1 = new Feet(1.0);
+
+        assertTrue(f1.equals(f1), "Object should be equal to itself");
+    }
+    
+    @Test
+    public void testInchesEquality_SameValue() {
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(1.0);
+
+        assertTrue(i1.equals(i2), "1.0 inch should be equal to 1.0 inch");
+    }
+    
+    @Test
+    public void testInchesEquality_DifferentValue() {
+    	Inches i1 = new Inches(1.0);
+    	Inches i2 = new Inches(2.0);
+
+        assertFalse(i1.equals(i2), "1.0 inch should not be equal to 2.0 inch");
+    }
+
+    @Test
+    public void testInchesEquality_NullComparison() {
+    	Inches i1 = new Inches(1.0);
+
+        assertFalse(i1.equals(null), "Inch object should not be equal to null");
+    }
+
+    @Test
+    public void testInchesEquality_DifferentClass() {
+    	Inches i1 = new Inches(1.0);
+        String other = "1.0";
+
+        assertFalse(i1.equals(other), "Inch object should not be equal to different type");
+    }
+
+    @Test
+    public void testInchesEquality_SameReference() {
+    	Inches i1 = new Inches(1.0);
+
+        assertTrue(i1.equals(i1), "Object should be equal to itself");
+    }
+}
