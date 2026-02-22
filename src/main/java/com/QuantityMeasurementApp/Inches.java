e01e3edbe890f3f3b58526e99b700c75418ea462
Dishagujar26 <dishagujar1@gmail.com>
2026-02-22 18:19:19 +0530

    [Disha Gujar] : Added inches program file

---
+++ src/main/java/com/QuantityMeasurementApp/Inches.java
@@ -0,0 +1,20 @@
+package com.QuantityMeasurementApp;
+
+public class Inches {
+
+	private final double value;
+
+	public Inches(double value) {
+		this.value = value;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null || getClass() != obj.getClass())
+			return false;
+		Inches inches = (Inches) obj;
+		return Double.compare(inches.value, value) == 0;
+	}
+}
