d31cadfc183a636f439da1f77d569ec00faaffef
Dishagujar26 <dishagujar1@gmail.com>
2026-02-23 12:15:33 +0530

    [Disha Gujr] : Implemented Addition with Target Unit Specification

--- src/main/java/com/QuantityMeasurementApp/LengthUnit.java
+++ src/main/java/com/QuantityMeasurementApp/LengthUnit.java
@@ -2,15 +2,16 @@ package com.QuantityMeasurementApp;
 
 public enum LengthUnit {
 
-
 	FEET(12.0), INCHES(1.0), YARDS(36.0), CENTIMETERS(0.393701);
-	private final double conversionFactor;
 
-	LengthUnit(double conversionFactor) {
-		this.conversionFactor = conversionFactor;
+	private final double toInchesFactor;
+
+	LengthUnit(double toInchesFactor) {
+		this.toInchesFactor = toInchesFactor;
 	}
 
-	public double getConversionFactor() {
-		return conversionFactor;
+	public double toInches(double value) {
+		return value * toInchesFactor;
 	}
+
 }
