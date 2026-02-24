0fdce167416c182d478c3d30c1fb4bbcbb74b5d3
Dishagujar26 <dishagujar1@gmail.com>
2026-02-24 10:42:44 +0530

    [Disha]: Modified File

--- src/main/java/com/QuantityMeasurementApp/QuantityMeasurementApp.java
+++ src/main/java/com/QuantityMeasurementApp/QuantityMeasurementApp.java
@@ -2,9 +2,11 @@ package com.QuantityMeasurementApp;
 
 public class QuantityMeasurementApp {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+
+		Feet f1 = new Feet(1.0);
+		Feet f2 = new Feet(1.0);
+
+		System.out.println("Are equal? " + f1.equals(f2));
+
 	}
 }
-
-
-
